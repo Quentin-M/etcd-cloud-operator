884977a74ab7992e857c7617690fb3d913e0e90d
Quentin MACHU <me@quentin-machu.fr>
2017-10-24 18:22:06 -0700

    terraform/platforms/aws: add cloudwatch alert for CPU

--- terraform/platforms/aws/aws.tf
+++ terraform/platforms/aws/aws.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "elb" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "elb" {
-  alarm_name = "${var.name}"
+  alarm_name = "elb.${var.name}"
 
   metric_name = "UnHealthyHostCount"
   namespace   = "AWS/ELB"
@@ -86,6 +86,27 @@ resource "aws_cloudwatch_metric_alarm" "elb" {
   statistic           = "Maximum"
   period              = 60
   evaluation_periods  = 5
+
+  dimensions {
+    LoadBalancerName = "${aws_elb.clients.name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu" {
+  alarm_name = "cpu.${var.name}"
+
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold = 80
+  statistic = "Average"
+  period = 60
+  evaluation_periods = 5
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.main.name}"
+  }
 }
 
 locals {
